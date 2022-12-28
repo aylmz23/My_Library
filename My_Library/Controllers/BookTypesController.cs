7bc19322a7e2813484db3d15183e26c88ea9d59d
aylmz <98480653+aylmz23@users.noreply.github.com>
2022-12-28 20:47:53 +0300

    2022-12-28-2

--- My_Library/Controllers/BookTypesController.cs
+++ My_Library/Controllers/BookTypesController.cs
@@ -50,7 +50,6 @@ namespace My_Library.Controllers
         {
             return View();
         }
-
         // POST: BookTypes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
